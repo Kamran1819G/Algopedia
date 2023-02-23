68335d6ef2d2261b758af67f56a83cad7737d16a
Kamran Khan <72748315+Kamran1819G@users.noreply.github.com>
2023-02-24 01:24:16 +0530

    updated

--- website/src/components/searchsystem/SearchBar.js
+++ website/src/components/searchsystem/SearchBar.js
@@ -1,48 +1,30 @@
 import React, {useState} from "react";
 import { Form, FloatingLabel, Button } from "react-bootstrap";
-import AllAlgorithms from "../../JSON/AllAlgorithms.json";
-import AlgorithmCard from "../algorithmcard/AlgorithmCard";
 
-const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-  
-    const handleSearch = (event) => {
-      const query = event.target.value;
-      setSearchQuery(query);
-  
-      const results = AllAlgorithms.filter((item) =>
-        item.name.toLowerCase().includes(query.toLowerCase())
-      );
-  
-      setSearchResults(results);
-    };
+
+
+const SearchBar = ({onSearch}) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    setQuery(query);
+    onSearch(query);
+  };
   return (
     <>
     <Form className="search-form d-flex">
       <FloatingLabel
         controlId="floatingInput"
         label="Search Algorithm"
-        className="mb-3 col-12"
+        className="mb-5 col-6"
       >
-        <Form.Control type="search" placeholder="Search Algorithm" value={searchQuery} onChange={handleSearch} />
+        <Form.Control type="text" placeholder="Search Algorithm" value={query} onChange={handleSearch}/>
       </FloatingLabel>
-      <Button className=" search-btn mb-3">
+      <Button className=" search-btn mb-5">
         <i className="bi bi-search"></i>
       </Button>
     </Form>
-    <div className="card-container d-flex justify-content-evenly">
-              {searchResults.map((item) => {
-                return (
-                        <AlgorithmCard
-                          key={item.id}
-                          title={item.title}
-                          category={item.category}
-                          url={item.url}
-                        />
-                );
-              })}
-              </div>
     </>
   );
 };
--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Home.css";
 import AlgorithmCard from "../components/algorithmcard/AlgorithmCard";
-import {
-  Container,
-  Row,
-  Col
-} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import PopularAlgorithms from "../JSON/PopularAlgorithms.json";
 import FeaturedAlgorithms from "../JSON/FeaturedAlgorithms.json";
+import AllAlgorithms from "../JSON/AllAlgorithms.json";
 import SearchBar from "../components/searchsystem/SearchBar";
 function Home() {
-  
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (query) => {
+    if (query.length >= 2) {
+      const matchedResults = AllAlgorithms.filter((algorithm) =>
+        algorithm.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setResults(matchedResults);
+    } else {
+      setResults([]);
+    }
+
+    if (query === "") {
+      setResults([]);
+    }
+  };
+
   return (
     <>
-      <section className="section h-100vh">
+      <section className="section intro-section">
         <Container fluid="lg">
           <Row>
             <Col lg="12">
@@ -25,7 +38,23 @@ function Home() {
               </div>
             </Col>
             <Col lg="12">
-              <SearchBar />
+              <SearchBar onSearch={handleSearch} />
+              {results.length > 0 && (
+                <Row xs={1} md={4} className="g-5">
+                  {results.map((item) => {
+                    return (
+                      <Col>
+                        <AlgorithmCard
+                          key={item.title}
+                          title={item.name}
+                          category={item.category}
+                          url={item.url}
+                        />
+                      </Col>
+                    );
+                  })}
+                </Row>
+              )}
             </Col>
           </Row>
         </Container>
@@ -38,16 +67,16 @@ function Home() {
                 <h1 className="title">Popular Algorithms</h1>
               </div>
               <div className="card-container d-flex justify-content-evenly">
-              {PopularAlgorithms.map((item) => {
-                return (
-                        <AlgorithmCard
-                          key={item.id}
-                          title={item.title}
-                          category={item.category}
-                          url={item.url}
-                        />
-                );
-              })}
+                {PopularAlgorithms.map((item) => {
+                  return (
+                    <AlgorithmCard
+                      key={item.id}
+                      title={item.title}
+                      category={item.category}
+                      url={item.url}
+                    />
+                  );
+                })}
               </div>
             </Col>
           </Row>
@@ -61,16 +90,16 @@ function Home() {
                 <h1 className="title">Featured Algorithms</h1>
               </div>
               <div className="card-container d-flex justify-content-evenly">
-              {FeaturedAlgorithms.map((item) => {
-                return (
-              <AlgorithmCard
-                key={item.id}
-                title={item.title}
-                category={item.category}
-                url={item.url}
-              />
-                );
-              })}
+                {FeaturedAlgorithms.map((item) => {
+                  return (
+                    <AlgorithmCard
+                      key={item.id}
+                      title={item.title}
+                      category={item.category}
+                      url={item.url}
+                    />
+                  );
+                })}
               </div>
             </Col>
           </Row>
