7ce517b7c4892fdf356665582a6ebfba1f21b5f1
Kamran Khan <kamrankhan.sde@gmail.com>
2023-04-10 18:35:42 +0530

    Mobile View Updated

--- website/src/components/searchsystem/SearchBar.js
+++ website/src/components/searchsystem/SearchBar.js
@@ -17,11 +17,11 @@ const SearchBar = ({onSearch}) => {
       <FloatingLabel
         controlId="floatingInput"
         label="Search Algorithm"
-        className="mb-5 col-6"
+        className="mb-5 col-lg-6 col-md-8 col-sm-10"
       >
         <Form.Control type="text" placeholder="Search Algorithm" value={query} onChange={handleSearch}/>
       </FloatingLabel>
-      <Button className=" search-btn mb-5">
+      <Button className=" search-btn mb-5 p-3">
         <i className="bi bi-search"></i>
       </Button>
     </Form>
