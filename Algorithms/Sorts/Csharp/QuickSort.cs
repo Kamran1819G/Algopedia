d9dda712b93f473d5c0dae142cef4b09cc861715
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-04-13 18:42:15 +0300

    Added C# QuickSort

---
+++ Algorithms/Sorts/Csharp/QuickSort.cs
@@ -0,0 +1,53 @@
+﻿public static class QuickSort
+{
+    public static void QSort(int[] array, int start, int end)
+    {
+        if (end <= start)
+            return;
+
+        int pivot = Partition(array, start, end);
+        QSort(array, start, pivot - 1);
+        QSort(array, pivot + 1, end);
+    }
+
+    private static int Partition(int[] array, int start, int end)
+    {
+        int pivot = array[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end; j++) {
+            if (array[j] < pivot) {
+                i++;
+                (array[i], array[j]) = (array[j], array[i]); // C# swapping two variables without temp variable
+            }
+        }
+        i++;
+        
+        (array[i], array[end]) = (array[end], array[i]);
+
+        return i;
+    }
+    
+    public static void Main()
+    {
+        int[] array = new int[]{ 9, 8, 7, 5, 4, 3, 2, 1};
+        
+        Console.WriteLine("Unsorted array:");
+        PrintArray(array);
+
+        QSort(array, 0, array.Length - 1);
+        
+        Console.WriteLine("Sorted array:");
+        PrintArray(array);
+
+    }
+
+    private static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine(";");
+    }
+}
