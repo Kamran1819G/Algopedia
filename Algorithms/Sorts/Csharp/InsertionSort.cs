884857a695d74cf6bb4bfd622c253a279509ab86
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-04-13 18:29:32 +0300

    Added C# Insertion sort

---
+++ Algorithms/Sorts/Csharp/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿public static class InsertionSort
+{
+    public static void InsSort(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            
+            array[j + 1] = temp;
+        }
+    }
+    
+    public static void Main()
+    {
+        int[] array = new int[]{ 9, 8, 7, 5, 4, 3, 2, 1};
+        
+        Console.WriteLine("Unsorted array:");
+        PrintArray(array);
+
+        InsSort(array);
+        
+        Console.WriteLine("Sorted array:");
+        PrintArray(array);
+
+    }
+
+    private static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine(";");
+    }
+}
