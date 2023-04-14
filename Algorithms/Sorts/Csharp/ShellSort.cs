94002995d19b94d09aa7919d309be78f60509132
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-04-14 15:30:23 +0300

    Added C# shell sort

---
+++ Algorithms/Sorts/Csharp/ShellSort.cs
@@ -0,0 +1,43 @@
+﻿public static class ShellSort
+{
+    public static void Sort(int[] array)
+    {
+        for (int gap = array.Length / 2; gap > 0; gap /= 2)
+        {
+            for (int i = gap; i < array.Length; i++)
+            {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+                {
+                    array[j] = array[j - gap];
+                }
+
+                array[j] = temp;
+            }
+        }
+    }
+    public static void Main()
+    {
+        int[] array = new int[]{ 9, 8, 7, 5, 4, 3, 2, 1};
+        
+        Console.WriteLine("Unsorted array:");
+        PrintArray(array);
+
+        
+        Sort(array);
+        
+        Console.WriteLine("Sorted array:");
+        PrintArray(array);
+
+    }
+
+    private static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine(";");
+    }
+}
